dcd41045426663106f7f62b4ea3fef3e6277a818
matias <matidae@gmail.com>
2016-04-30 23:48:33 -0300

    check if file exists

--- getseq.py
+++ getseq.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import argparse
+import os.path
 from Bio import SeqIO
 
 def printSixty(seq):
@@ -31,18 +32,17 @@ def printSeq(seq):
         print seq
 
 parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
 parser.add_argument("fasta", help="Fasta file")
-group.add_argument("seqs", help="File with sequence names or comma separated sequence names")
+parser.add_argument("seqs", help="File with sequence names or comma separated sequence names")
 parser.add_argument("-u", help="Print each sequence in a single line", action="store_true")
 parser.add_argument("-r", help="Print reverse complement sequence", action="store_true")
 parser.add_argument("-l", help="Print length and name of a sequence", action="store_true")
-group.add_argument("-a", help="Use all sequences in fasta file, sequence names not needed", action="store_true")
+parser.add_argument("-a", help="Use all sequences in fasta file, sequence names not needed", action="store_true")
 args = parser.parse_args()
 
 if not args.a:
     data = SeqIO.index(args.fasta, "fasta")
-    if args.seqs.count(",") == 0:
+    if os.path.isfile(args.seqs):
         with open(args.seqs) as listseq:
             for i in listseq:
                 if args.l:
