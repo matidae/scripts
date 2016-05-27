2d6ed4be46786d7df65fde4be3a0a5d8b3f832df
matias <matidae@gmail.com>
2016-05-26 23:42:14 -0300

    min

--- char_freq.py
+++ char_freq.py
@@ -1,6 +1,8 @@
 import urllib, json, re, random, string
+from itertools import permutations
 
 word = "Book"
+sample_size = 150
 langs = ['cs', 'da', 'de', 'en', 'es', 'eu', 'fi', 'fr', 'hu', 'is', 'it', 'la', 'nl', 'no', 'pl', 'pt', 'ro', 'sk', 'sl', 'sv']
 
 def get_interlangs(word, langs):
@@ -18,9 +20,7 @@ def get_interlangs(word, langs):
             dict_langs[i] = labels[i]['value']
     return dict_langs
 
-dict_langs = get_interlangs(word, langs)
-
-def get_some_words(word, lang):
+def get_some_words(word, lang, sample_size):
     urltext = "https://" + lang + ".wikipedia.org/w/api.php?action=query&prop=revisions&titles=" + word + "&rvprop=content&format=json"
     response_text = urllib.urlopen(urltext)
     json_text = json.load(response_text)
@@ -33,23 +33,39 @@ def get_some_words(word, lang):
     for word in list_text:
         if len(word)>3 and re.match('^[a-z]*$', word.lower()):
             filter_text.append(word.lower())
-    return random.sample(set(filter_text), 10)
+        if len(set(filter_text)) >= sample_size:
+            return random.sample(set(filter_text), sample_size)
 
-dict_words = {}
-for l in langs:
-    if l in dict_langs.keys():
-        word_lang = dict_langs[l]
-        word_list = get_some_words(word_lang.encode("utf-8"), l)
-        dict_words[l] = word_list
-print  dict_words
+def sample_dict_words(langs, dict_langs, sample_size):
+    dict_words = {}
+    for l in langs:
+        if l in dict_langs.keys():
+            word_lang = dict_langs[l]
+            word_list = get_some_words(word_lang.encode("utf-8"), l, sample_size)
+            dict_words[l] = word_list
+    return  dict_words
 
-char_dict = {i:0 for i in string.ascii_lowercase}
-from itertools import permutations
-l = list(string.ascii_lowercase * 1)
-kwords = []
-for i in sorted(set(permutations(l,1))):
-    kwords.append("".join(i))
+def generate_kwords(num):
+    l = list(string.ascii_lowercase * num)
+    kwords = []
+    for i in sorted(set(permutations(l, num))):
+        kwords.append("".join(i))
+    return kwords
 
+def generate_freq_vector(kdict, word_list):
+    klist = [0] * len(kdict)
+    for word in word_list:
+        for char in word:
+            klist[kdict[char]] += 1
+    return klist
 
+dict_langs = get_interlangs(word, langs)
+dict_words = sample_dict_words(langs, dict_langs, sample_size)
+kwords= generate_kwords(1)
+kdict = {i:k for k,i in enumerate(kwords)}
 
-
+for k in dict_words:
+        if dict_words[k]:
+            kvector = generate_freq_vector(kdict, dict_words[k])
+            print k +',' + ','.join(map(str, kvector))
+        
