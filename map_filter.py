c7cbdf619d0d6e6db82e0e9dca3766e152c1dce4
Matias <matidae@gmail.com>
2016-07-12 23:54:52 -0300

    test

--- map_filter.py
+++ map_filter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 
+
 class Read(object):
     def __init__(self, name):
         self.name = name
@@ -18,11 +19,23 @@ class Mapping(object):
         self.mate_distance = 0
 
 
-def main (mapping_file):
+def main(mapping_file):
+    name_aux = ""
+    first = True
     with open(mapping_file) as mf:
         for line in mf:
             name = line.split()[0]
+            if first:
+                name_aux = name
+                first = False
+            elif name == name_aux:
+                print 1, name
+            else:
+                print name
+                name_aux = name
+        print name
 
-    
+
 if __name__ == "__main__":
     mapping_file = sys.argv[1]
+    main(mapping_file)
